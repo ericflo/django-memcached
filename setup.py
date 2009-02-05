1e6a2f77fc03f5cbc449db70df0223d822fb610b
Eric Florenzano <floguy@gmail.com>
2009-02-05 01:37:26 -0800

    Updated documentation to be proper ReST format.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
  
-version = '0.1'
+version = '0.1.1'
  
 LONG_DESCRIPTION = """
 django-memcached
@@ -16,24 +16,28 @@ Installing django-memcached
 1. Either download the tarball and run ``python setup.py install``, or simply
    use easy install or pip like so ``easy_install django-memcached``.
 
-2. Add django_memcached to your ``INSTALLED_APPS`` setting:
+
+2. Add django_memcached to your ``INSTALLED_APPS`` setting::
 
        INSTALLED_APPS = (
            # ...
            'django_memcached',
        )
 
-3. Add the urls to your url configuration:
+
+3. Add the urls to your url configuration::
 
        urlpatterns = patterns('',
            # ...
            (r'^cache/', include('django_memcached.urls')),
        )
 
+
 4. If you want to restrict these urls to only staff members, just add this
-   setting to your settings file:
+   setting to your settings file::
    
        DJANGO_MEMCACHED_REQUIRE_STAFF = True
+
 
 That's it.  Hopefully this app gives you a little insight into how your
 memcached servers are being utilized.
